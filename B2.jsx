6ed4da1fe3fb2bb5403773df6e58853b938067f9
dschalk <schalkbox@gmail.com>
2015-09-22 09:08:17 -0400

    Streamlined and corrected flaws in B2.jsx.

--- B2.jsx
+++ B2.jsx
@@ -15,7 +15,7 @@ export {B2};
       return
     } else {
       if( event.keyCode == 13 ) {
-        this.props.setGroup(group);
+        this.props.data.group = group;
       }
     }
   }
@@ -24,7 +24,7 @@ export {B2};
     if (group == '') {
         return
     } else {
-      this.props.setGroup(group);
+        this.props.data.group = group;
     }
   }
   render = () => {
@@ -39,237 +39,61 @@ export {B2};
   }
 };
 
-let mouseHandler = mobservable.makeReactive({
-    Abackground: '#000',
-    Aborder: 'green',
-    Acolor: 'burlywood',
-    Asize: 12,
-    Bbackground: '#000',
-    Bborder: 'green',
-    Bcolor: 'burlywood',
-    Bsize: 12,
-    Cbackground: '#000',
-    Cborder: 'green',
-    Ccolor: 'burlywood',
-    Csize: 12,
-    Sbackground: '#000',
-    Sborder: 'green',
-    Scolor: 'burlywood',
-    Ssize: 12,
-    rSbackground: 'green',
-    rSborder: 'lightgreen',
-    rScolor: 'yellow',
-    27: '#000',
-    270: 'darkred',
-    28: 'burlywood',
-    37: '#000',
-    370: 'darkred',
-    38: 'burlywood',
-    47: '#000',
-    470: 'darkred',
-    48: 'burlywood',
-    57: '#000',
-    570: 'darkred',
-    58: 'burlywood',
-    67: '#000',
-    670: 'darkred',
-    68: 'burlywood',
-    77: '#000',
-    770: 'darkred',
-    78: 'burlywood',
-    87: '#000',
-    870: 'darkred',
-    88: 'burlywood',
-    97: '#000',
-    970: 'darkred',
-    98: 'burlywood',
-    107: '#000',
-    1070: 'darkred',
-    108: 'burlywood',
-    117: '#000',
-    1170: 'darkred',
-    118: 'burlywood',
-    127: '#000',
-    1270: 'darkred',
-    128: 'burlywood',
-    137: '#000',
-    1370: 'darkred',
-    138: 'burlywood'
-});
-
 let data = mobservable.makeReactive({
   group: 'solo',
-  name: '',
-  test: true,
+  Ahover: false,
+  Afocus: false,
+  Bhover: false,
+  Bfocus: false,
+  Chover: false,
+  Cfocus: false,
+  Shover: false,
+  Sfocus: false,
+  style1: { a: '#000', b: 'green', c: '#FFE4C4' },
+  style2: { a: 'blue', b: 'skyblue', c: 'yellow' },
+  fA: function() {
+    if (this.Ahover || this.group === 'GroupA') {
+      return this.style2;
+    }
+    return this.style1;
+  },
+  fB: function() {
+    if (this.Bhover || this.group === 'GroupB') {
+      return this.style2;
+    }
+    return this.style1;
+  },
+  fC: function() {
+    if (this.Chover || this.group === 'GroupC') {
+      return this.style2;
+    }
+    return this.style1;
+  },
+  fS: function() {
+    if (this.Shover || this.group === 'solo') {
+      return this.style2;
+    }
+    return this.style1;
+  }
 });
-
- data.groupWatch = function() {
-    if (this.group === 'GroupA' && this.test) {
-      mouseHandler.Abackground = 'green';
-      mouseHandler.Aborder = 'lawngreen';
-      mouseHandler.Acolor = 'yellow';
-      mouseHandler.Bbackground = '#000';
-      mouseHandler.Bborder = 'green';
-      mouseHandler.Bcolor = 'burlywood';
-      mouseHandler.Cbackground = '#000';
-      mouseHandler.Cborder = 'green';
-      mouseHandler.Ccolor = 'burlywood';
-      mouseHandler.Sbackground = '#000';
-      mouseHandler.Sborder = 'green';
-      mouseHandler.Scolor = 'burlywood';
-    }
-    else if (this.group === 'GroupB' && this.test) {
-      mouseHandler.Bbackground = 'green';
-      mouseHandler.Bborder = 'lawngreen';
-      mouseHandler.Bcolor = 'yellow';
-      mouseHandler.Abackground = '#000';
-      mouseHandler.Aborder = 'green';
-      mouseHandler.Acolor = 'burlywood';
-      mouseHandler.Cbackground = '#000';
-      mouseHandler.Cborder = 'green';
-      mouseHandler.Ccolor = 'burlywood';
-      mouseHandler.Sbackground = '#000';
-      mouseHandler.Sborder = 'green';
-      mouseHandler.Scolor = 'burlywood';
-    }
-    else if (this.group === 'GroupC' && this.test) {
-      mouseHandler.Cbackground = 'green';
-      mouseHandler.Cborder = 'lawngreen';
-      mouseHandler.Ccolor = 'yellow';
-      mouseHandler.Abackground = '#000';
-      mouseHandler.Aborder = 'green';
-      mouseHandler.Acolor = 'burlywood';
-      mouseHandler.Bbackground = '#000';
-      mouseHandler.Bborder = 'green';
-      mouseHandler.Bcolor = 'burlywood';
-      mouseHandler.Sbackground = '#000';
-      mouseHandler.Sborder = 'green';
-      mouseHandler.Scolor = 'burlywood';
-    }
-    else if (this.group === 'solo' && this.test) {
-      mouseHandler.Sbackground = 'green';
-      mouseHandler.Sborder = 'lawngreen';
-      mouseHandler.Scolor = 'yellow';
-      mouseHandler.Abackground = '#000';
-      mouseHandler.Aborder = 'green';
-      mouseHandler.Acolor = 'burlywood';
-      mouseHandler.Bbackground = '#000';
-      mouseHandler.Bborder = 'green';
-      mouseHandler.Bcolor = 'burlywood';
-      mouseHandler.Cbackground = '#000';
-      mouseHandler.Cborder = 'green';
-      mouseHandler.Ccolor = 'burlywood';
-    }
-    else if (this.test) {
-      mouseHandler.Abackground = '#000';
-      mouseHandler.Aborder = 'green';
-      mouseHandler.Acolor = 'burlywood';
-      mouseHandler.Bbackground = '#000';
-      mouseHandler.Bborder = 'green';
-      mouseHandler.Bcolor = 'burlywood';
-      mouseHandler.Cbackground = '#000';
-      mouseHandler.Cborder = 'green';
-      mouseHandler.Ccolor = 'burlywood';
-      mouseHandler.Sbackground = '#000';
-      mouseHandler.Sborder = 'green';
-      mouseHandler.Scolor = 'burlywood';
-    }
- }
 
 @reactiveComponent class B2 extends React.Component {
   constructor(props) {
     super(props);
-    this.mouse = mouseHandler;
     this.data = data;
-    this.data.groupWatch();
   }
 
 
   style8 = (x,y,z) => {return {backgroundColor: x, textAlign: 'left', borderColor: y, outline: 0,
     color: z, borderRadius: 10, paddingTop: 1.1, paddingBottom: 0.9, marginRight: 3, marginLeft: 12, fontSize: 20 }};
 
-  style9 = (w,x,y,z) => {return {display: w, backgroundColor: x, textAlign: 'left', borderColor: y, outline: 0,
-    color: z, borderRadius: 10, paddingTop: 1.1, paddingBottom: 0.9, marginRight: 3, marginLeft: 12, fontSize: 20 }};
-
   render = () => {
-    console.log(this);
-    let warning = this.data.warning;
-    let message = this.data.message;
-    let increaseX = this.data.increaseX;
-    let increaseW = this.data.increaseW;
     let group = this.data.group;
-    let Abackground = '#000';
-    let Aborder = 'green';
-    let Acolor = 'burlywood';
-    let Bbackground = '#000';
-    let Bborder = 'green';
-    let Bcolor = 'burlywood';
-    let Cbackground = '#000';
-    let Cborder = 'green';
-    let Ccolor = 'burlywood';
-    let Sbackground = '#000';
-    let Sborder = 'green';
-    let Scolor = 'burlywood';
-    let Ubackground = '#000';
-    let Uborder = 'green';
-    let Ucolor = 'burlywood';
-    let Tbackground = '#000';
-    let Tborder = 'green';
-    let Tcolor = 'burlywood';
-    let rAbackground = 'green';
-    let rAborder = 'lightgreen';
-    let rAcolor = 'yellow';
-    let rBbackground = 'green';
-    let rBborder = 'lightgreen';
-    let rBcolor = 'yellow';
-    let rCbackground = 'green';
-    let rCborder = 'lightgreen';
-    let rCcolor = 'yellow';
-    let rSbackground = 'green';
-    let rSborder = 'lightgreen';
-    let rScolor = 'yellow';
-    let cr27 = this.mouse[27];
-    let cr270 = this.mouse[270];
-    let cr28 = this.mouse[28];
-    let cr37 = this.mouse[37];
-    let cr370 = this.mouse[370];
-    let cr38 = this.mouse[38];
-    let cr47 = this.mouse[47];
-    let cr470 = this.mouse[470];
-    let cr48 = this.mouse[48];
-    let cr57 = this.mouse[57];
-    let cr570 = this.mouse[570];
-    let cr58 = this.mouse[58];
-    let cr67 = this.mouse[67];
-    let cr670 = this.mouse[670];
-    let cr68 = this.mouse[68];
-    let cr77 = this.mouse[77];
-    let cr770 = this.mouse[770];
-    let cr78 = this.mouse[78];
-    let cr87 = this.mouse[87];
-    let cr870 = this.mouse[870];
-    let cr88 = this.mouse[88];
-    let cr97 = this.mouse[97];
-    let cr970 = this.mouse[970];
-    let cr98 = this.mouse[98];
-    let cr107 = this.mouse[107];
-    let cr1070 = this.mouse[1070];
-    let cr108 = this.mouse[108];
-    let cr117 = this.mouse[117];
-    let cr1170 = this.mouse[1170];
-    let cr118 = this.mouse[118];
-    let cr127 = this.mouse[127];
-    let cr1270 = this.mouse[1270];
-    let cr128 = this.mouse[128];
-    let cr137 = this.mouse[137];
-    let cr1370 = this.mouse[1370];
-    let cr138 = this.mouse[138];
-
-    return (
+       return (
       <div style={{ backgroundColor: '#000', height: 3800, width: '100%', color: '#FFE4C4' }}>
         <br /><br /><br />
           <h1 style={{textAlign: 'center'}} >Reactive Buttons</h1>
-        <div style={{width: '80%', marginLeft: 85, float: 'right', marginRight: 2}} >
+        <div style={{width: '80%', marginLeft: 85, float: 'right', marginRight: 2, color: '#FFE4C4' }} >
           <br />
         Current Group:
       <button style={this.style8('blue', 'lightgreen', 'red')} >
@@ -278,128 +102,63 @@ let data = mobservable.makeReactive({
         <br /><br />
           <button onClick={() => {
                   this.data.group = 'GroupA';
-                  this.data.test = true;
-    this.data.groupWatch();
-          }}
+            }}
             onMouseEnter={() => {
-              if (group !== 'GroupA') {
-                  this.data.test = false;
-                  this.mouse.Abackground = rAbackground;
-                  this.mouse.Aborder  = rAborder;
-                  this.mouse.Acolor = rAcolor;
-    this.data.groupWatch();
-                }
-              }
-            }
+                  this.data.Ahover = true;
+            }}
             onMouseLeave={() => {
-              if (group !== 'GroupA') {
-                  this.data.test = true;
-                  this.mouse.Abackground = Abackground;
-                  this.mouse.Aborder  = Aborder;
-                  this.mouse.Acolor = Acolor;
-    this.data.groupWatch();
-                }
+                  this.data.Ahover = false;
               }
             }
-              style={this.style8(this.mouse.Abackground,this.mouse.Aborder,this.mouse.Acolor)} >
+            style={this.style8(this.data.fA.a,this.data.fA.b,this.data.fA.c)} >
             GroupA
           </button>
-
           <button onClick={() => {
                   this.data.group = 'GroupB';
-                  this.data.test = true;
-    this.data.groupWatch();
-          }}
+            }}
             onMouseEnter={() => {
-              if (group !== 'GroupB') {
-                  this.data.test = false;
-                  this.mouse.Bbackground = rBbackground;
-                  this.mouse.Bborder  = rBborder;
-                  this.mouse.Ncolor = rBcolor;
-    this.data.groupWatch();
-                }
-              }
-            }
+                  this.data.Bhover = true;
+            }}
             onMouseLeave={() => {
-              if (group !== 'GroupB') {
-                  this.data.test = true;
-                  this.mouse.Bbackground = Bbackground;
-                  this.mouse.Bborder  = Bborder;
-                  this.mouse.Bcolor = Bcolor;
-    this.data.groupWatch();
-                }
+                  this.data.Bhover = false;
               }
             }
-              style={this.style8(this.mouse.Bbackground,this.mouse.Bborder,this.mouse.Bcolor)} >
+            style={this.style8(this.data.fB.a,this.data.fB.b,this.data.fB.c)} >
             GroupB
           </button>
-
           <button onClick={() => {
                   this.data.group = 'GroupC';
-                  this.data.test = true;
-    this.data.groupWatch();
-          }}
+            }}
             onMouseEnter={() => {
-              if (group !== 'GroupC') {
-                  this.data.test = false;
-                  this.mouse.Cbackground = rCbackground;
-                  this.mouse.Cborder  = rCborder;
-                  this.mouse.Ccolor = rCcolor;
-    this.data.groupWatch();
-                }
-              }
-            }
+                  this.data.Chover = true;
+            }}
             onMouseLeave={() => {
-              if (group !== 'GroupC') {
-                  this.data.test = true;
-                  this.mouse.Cbackground = Cbackground;
-                  this.mouse.Cborder  = Cborder;
-                  this.mouse.Ccolor = Ccolor;
-    this.data.groupWatch();
-                }
+                  this.data.Chover = false;
               }
             }
-              style={this.style8(this.mouse.Cbackground,this.mouse.Cborder,this.mouse.Ccolor)} >
+            style={this.style8(this.data.fC.a,this.data.fC.b,this.data.fC.c)} >
             GroupC
           </button>
-
           <button onClick={() => {
                   this.data.group = 'solo';
-                  this.data.test = true;
-    this.data.groupWatch();
-          }}
+            }}
             onMouseEnter={() => {
-              if (group !== 'solo') {
-                  this.data.test = false;
-                  this.mouse.Sbackground = rSbackground;
-                  this.mouse.Sborder  = rSborder;
-                  this.mouse.Scolor = rScolor;
-    this.data.groupWatch();
-                }
-              }
-            }
+                  this.data.Shover = true;
+            }}
             onMouseLeave={() => {
-              if (group !== 'solo') {
-                  this.data.test = true;
-                  this.mouse.Sbackground = Sbackground;
-                  this.mouse.Sborder  = Sborder;
-                  this.mouse.Scolor = Scolor;
-    this.data.groupWatch();
-                }
+                  this.data.Shover = false;
               }
             }
-              style={this.style8(this.mouse.Sbackground,this.mouse.Sborder,this.mouse.Scolor)} >
+            style={this.style8(this.data.fS.a,this.data.fS.b,this.data.fS.c)} >
             solo
           </button>
-          <br /><br />
-          <GroupNew key='GroupNew'  setGroup={(x) => {this.data.group = x; this.data.groupWatch() }} name={this.data.name} />
-<br /><br />
 
-  </div>
-</div>
-    )}
-  };
+            <br /><br />
+          <GroupNew key='GroupNew' data={this.data} />
+            <br /><br />
 
-reactMixin(B2.prototype, require('./node_modules/react/lib/AutoFocusMixin'));
-
+        </div>
+      </div>
+       )}
+}
 React.render(<B2 key='B2' />, document.getElementById('divSix'));
